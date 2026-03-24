7a733a73e1b412e209c2edb639ebb07f9a633f96
Jatin <84621253+h0x0er@users.noreply.github.com>
2026-03-24 19:32:50 +0530

    update

--- litellm.sh
+++ litellm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -x
 
-CGROUP_PATH="/sys/fs/cgroup/litellm_limit"
-sudo mkdir -p "$CGROUP_PATH"
-echo $((512 * 1024 * 1024)) | sudo tee "$CGROUP_PATH/memory.max"
-echo $$ | sudo tee "$CGROUP_PATH/cgroup.procs"
+# CGROUP_PATH="/sys/fs/cgroup/litellm_limit"
+# sudo mkdir -p "$CGROUP_PATH"
+# echo $((512 * 1024 * 1024)) | sudo tee "$CGROUP_PATH/memory.max"
+# echo $$ | sudo tee "$CGROUP_PATH/cgroup.procs"
 
 python3 -c "print('hello')"
 
